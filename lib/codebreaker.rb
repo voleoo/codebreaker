3d853a4df266bd358ed679e2a7a569f3224fd5cf
Viktor Leonets <4405511@gmail.com>
2013-06-01 12:59:06 +0300

    That’s incredible, works

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,3 +1,4 @@
+#coding: utf-8
 require "codebreaker/version"
 
 module Codebreaker
@@ -16,25 +17,26 @@ module Codebreaker
 
     def cycle_game
       @count = 0
-      while @count < 5 do
+      @iterations
+      while @count < @iterations do
         @input = gets.chomp
-        if @input == "hint" or @input == "h"
-          @output.puts "подсказочка"
-          next
-        end
-        unless input_validate? 
-          @output.puts 'Please enter code of four numbers between 1 and 6'
-          next
-        end
 
-        #@output.puts 'Загаданое число' + @guess_number
-        #@output.puts 'Введенное число' + @input
-        string_result = get_string_result(@input.split(//))
+      if @input == "hint" or @input == "h" 
+        @output.puts "hint_hint_hint"
+        next
+      end
+
+      unless input_validate? 
+        @output.puts 'Please enter code of four numbers between 1 and 6'
+        next
+      end
+
+        @string_result = get_string_result(@input.split(//))
         
-        if string_result == "++++"
+        if "++++" == @string_result
           winer(true)
         else
-          @output.puts string_result
+          @output.puts @string_result
         end
 
         @count += 1
@@ -42,28 +44,37 @@ module Codebreaker
       @count -= 1
       winer(false)
     end
-    
+
     def winer(winer)
       name = get_user_name
       if winer
         @output.puts " "
         @output.puts "#{name} you're winner!"
-        @output.puts 'Guess number ' + @guess_number
-        @output.puts 'Введенное number ' + @input
+        @output.puts "Guess number " + @guess_number
+        @output.puts "Inputed number " + @input
         @output.puts "Try again?"
       else
         @output.puts "Game ower!"
         @output.puts "Try again?"
       end
+
       save_result({ 
         :winer => winer, :name => name, :guess_number => @guess_number, 
         :last_input => @input, :step => @count+1})
 
+      start_game
+    end
+
+    def start_game
       if (gets.chomp == "yes")
         Game.new().start
       else
         exit
       end
+    end
+
+    def save_result(hash, file = "game_stat.txt")
+      File.open(file, "a") { |file| file.puts(hash) }
     end
 
     def get_user_name
@@ -72,13 +83,8 @@ module Codebreaker
       if (name.match /\w/)
         name
       else
-        get_user_nam
+        get_user_name
       end
-    end
-
-    private
-    def save_result(hash)
-      File.open("game_stat.txt", "a") { |file| file.puts(hash) }
     end
 
     def get_string_result(array)
@@ -108,4 +114,3 @@ module Codebreaker
   end
 end
 #Codebreaker::Game.new().start
-#@output.puts 'Please enter a number between 1111 and 6666' 
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -1,48 +1,146 @@
+#coding: utf-8
 require 'spec_helper'
 
 module Codebreaker
   describe Game do
 
-
     describe "#start" do
-      let(:output) { mock('output').as_null_object }
-      let(:cycle_game) { mock('cycle_game').as_null_object }      
+      let(:output) { mock('output').as_null_object }   
       let(:game)   { Game.new(output) }
 
       it "sends a welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
+        game.stub(:cycle_game).and_return('')
         game.start
       end
 
       it "prompts for the first guess" do
         output.should_receive(:puts).with('Enter guess:')
+        game.stub(:cycle_game).and_return('')
         game.start
       end
     end
 
-    describe "#cycle_game" do
-        it "message" do
+    describe "#get_user_name" do
+      let(:output) { mock('output').as_null_object }   
+      let(:game)   { Game.new(output) }
+        
+        it "you are prompted for the name" do
+          output.should_receive(:puts).with('Please enter your name:')
+          game.get_user_name
         end
+
+        it "valid name" do
+          game.stub(:gets).and_return("Vasya Pupkin")
+          game.get_user_name.should include("Vasya Pupkin")
+        end
+
+        it "not valid name"
+    end
+
+    describe "#cycle_game" do
+      let(:output) { mock('output').as_null_object }   
+      let(:game)   { Game.new(output) }
+
+        it "Show a preliminary result" do
+          game.instance_variable_set( :@guess_number, "1234")
+          game.instance_variable_set( :@iterations, 1)
+          game.stub(:winer).and_return()
+          game.stub(:gets).and_return("4321")
+
+          output.should_receive(:puts).with('----')
+          game.cycle_game
+        end
+
+        it "Show hint"
+        it "uncorrect input"
     end
 
     describe "#winer" do
-        it "message"
-    end
+      let(:output) { mock('output').as_null_object }   
+      let(:game)   { Game.new(output) }
+      before(:each) { 
+          game.instance_variable_set( :@guess_number, "1234")
+          game.instance_variable_set( :@input, "1234")
+          game.instance_variable_set( :@count, 4)
+          game.stub(:gets).and_return("Vasya Pupkin")
+          game.stub(:save_result).and_return()
+          game.stub(:exit).and_return()
+      }
+        it "Space string" do
+          output.should_receive(:puts).with(' ')
+          game.winer(true)
+        end
 
-    describe "#get_user_name" do
-        it "message"
-    end
+        it "Name winner" do
+          output.should_receive(:puts).with("Vasya Pupkin you're winner!")
+          game.winer(true)
+        end
 
-    describe "#save_result" do
-        it "message"
+        it "Guess number" do
+          output.should_receive(:puts).with("Guess number 1234")
+          game.winer(true)
+        end
+
+        it "Inputed number" do
+          output.should_receive(:puts).with("Inputed number 1234")
+          game.winer(true)
+        end
+
+        it "Try again?" do
+          output.should_receive(:puts).with("Try again?")
+          game.winer(true)
+        end
+
+        it "False Game ower!" do
+          output.should_receive(:puts).with("Game ower!")
+          game.winer(false)
+        end
+
+        it "False Try again?" do
+          output.should_receive(:puts).with("Try again?")
+          game.winer(false)
+        end
+
     end
 
     describe "#get_string_result" do
-        it "message"
+      let(:game)   { Game.new() }
+      before(:each) { 
+        game.instance_variable_set( :@guess_number, "1234")
+      }
+        it "returned all ++++" do
+          game.get_string_result(['1', '2', '3', '4']).should include("++++")
+        end
+        it "returned all ----" do
+            game.get_string_result(['4', '3', '2', '1']).should include("----")
+        end
+        it "returned mix -+--" do
+          game.get_string_result(['4', '2', '4', '1']).should include("-+--")
+        end
+        it "returned mix whis space +-" do
+          game.get_string_result(['1', '3', '5', '6']).should include("+-")
+        end
     end
 
     describe "#get_guess_number" do
-        it "message"
+      let(:game)   { Game.new() }
+        it "Return guess_number" do
+          game.get_guess_number.should match(/^[1-6]{4}$/)
+        end
+    end
+
+    describe "#start_game" do
+        it "I want to play more"
+    end
+
+    describe "#save_result" do
+      let(:game)   { Game.new() }
+        it "save_result" do
+          game.save_result("test string", "test_save_result.txt")
+          IO.read("test_save_result.txt").should include("test string")
+          File.delete("test_save_result.txt")
+        end
     end
   end
 end
